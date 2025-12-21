24b88c188fa7a200264374690bcbeb9176d67d51
Owen Buckley <owenbuckley13@gmail.com>
2025-12-20 19:28:59 -0500

    bump greenwood v0.34.0-alpha.2

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -1,7 +1,7 @@
 import { createTRPCProxyClient, httpBatchLink, loggerLink } from '@trpc/client';
 import type { AppRouter } from './router.ts';
 
-const url: URL = new URL('http://localhost:1984/api/rpc');
+const url: URL = new URL(`${window.location.origin}/api/rpc`);
 
 const client = createTRPCProxyClient<AppRouter>({
   links: [loggerLink(), httpBatchLink({ url })],
