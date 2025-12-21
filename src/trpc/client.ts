b73c837a4088d45af9de41f670e9d9f42d370841
Owen Buckley <owenbuckley13@gmail.com>
2025-12-20 19:32:47 -0500

    vercel adapter

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -1,6 +1,8 @@
 import { createTRPCProxyClient, httpBatchLink, loggerLink } from '@trpc/client';
 import type { AppRouter } from './router.ts';
 
+// TODO: would be great to get this from Greenwood
+// https://github.com/ProjectEvergreen/greenwood/discussions/1530
 const url: URL = new URL(`${window.location.origin}/api/rpc`);
 
 const client = createTRPCProxyClient<AppRouter>({
