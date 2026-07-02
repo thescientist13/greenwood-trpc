da6e701d0e336e8bdb34361af426f11d5cce9950
Owen Buckley <owenbuckley13@gmail.com>
2026-07-01 22:39:55 -0400

    feat: add modal on planet click and add trpc favicon

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,36 @@
 import { client } from './trpc/client.ts'
 
-const planets = await client.planets.listPlanets.query();
 
-console.log({ client, planets });
+document.addEventListener('DOMContentLoaded', async () => {
+  const planets = await client.planets.listPlanets.query();
+  const modal: HTMLDialogElement | null = document.querySelector('dialog');
+  const closeButton = modal?.querySelector('button');
+  console.log({ client, planets });
 
-if (planets) {
-  const planetsList = document.getElementById(
-    "planets-list",
-  ) as HTMLOListElement;
-
-  planets.forEach((planet) => {
-    const item = document.createElement("li") as HTMLLIElement;
-
-    item.textContent = planet.name;
-    item.addEventListener('click', async () => {
-      const p = await client.planets.getPlanetById.query(planet.id);
-
-      console.log('You clicked planet => ', { p });
-    })
-
-    planetsList.appendChild(item);
+  closeButton?.addEventListener('click', () => {
+    modal?.close();
   });
-}
+
+  if (planets) {
+    const planetsList = document.getElementById(
+      "planets-list",
+    ) as HTMLOListElement;
+
+    planets.forEach((planet) => {
+      const item = document.createElement("li") as HTMLLIElement;
+
+      item.textContent = planet.name;
+      item.addEventListener('click', async () => {
+        const p = await client.planets.getPlanetById.query(planet.id);
+        console.log('You clicked planet => ', { p });
+
+        if (modal) {
+          modal.querySelector('#content')!.textContent = p?.description ?? '';
+          modal.showModal();
+        }
+      })
+
+      planetsList.appendChild(item);
+    });
+  }
+});
