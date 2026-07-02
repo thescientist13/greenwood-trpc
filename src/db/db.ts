6b3ffb0a77420bda2e2be8da6b36b777b4d73e14
Owen Buckley <owenbuckley13@gmail.com>
2026-07-02 09:40:35 -0400

    chore: clean up comment

--- src/db/db.ts
+++ src/db/db.ts
@@ -10,17 +10,7 @@ type Planet = {
   description: string;
 }
 
-// const PlanetSchema = z.array(z.object({
-//   id: z.number().int().min(1),
-//   position: z.number().int().min(1),
-//   name: z.string(),
-//   description: z.string(),
-//   velocity: z.number().int(),
-//   distance: z.number().int(),
-//   image: z.string(),
-// }));
-
-// fake data
+// data source
 const planets = (await import("./planets.json", { with: { type: "json" } })).default;
 
 // start an in memory DB
